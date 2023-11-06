a94b8028b6081053f48e68db5ef38bde622a77e6
monzer <danesh.monzer@gmail.com>
2023-11-06 19:56:19 +0330

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function page() {
+  return <div>Invoices page</div>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function page() {
+  return <p>Dashboard page</p>;
+}
