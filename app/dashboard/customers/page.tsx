74b6d64aa6f2e0f4e28b7ddb2d1df188581ac864
monzer <danesh.monzer@gmail.com>
2023-11-08 18:07:26 +0330

    Auth and metadata added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
 /** @format */
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 
 export default function page() {
   return <div>Customers page</div>;
