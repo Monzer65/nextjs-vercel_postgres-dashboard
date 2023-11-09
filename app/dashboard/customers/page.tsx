239b6e3fca0d246d01951f6bebf5ca234ffd6b41
monzer <danesh.monzer@gmail.com>
2023-11-09 16:24:13 +0330

    updated customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,23 @@
 /** @format */
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function page() {
-  return <div>Customers page</div>;
+export default async function page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <CustomersTable query={query} />
+    </div>
+  );
 }
